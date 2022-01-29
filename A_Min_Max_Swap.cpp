37c913656c07aa992f4c7f39974a084b7d9fcb8a
chetan-skc <goyalchetan118@gmail.com>
2022-01-29 19:54:06 +0530

    updates

---
+++ A_Min_Max_Swap.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0)
+    
+int main()
+{
+    FIO;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        int b[n];
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+        }
+        for(int i=0; i<n; i++){
+            cin >> b[i];
+        }
+        
+    }
+    return 0;
+}
